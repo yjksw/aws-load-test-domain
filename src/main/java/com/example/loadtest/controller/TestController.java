01b407eddf7a26f9117bdbec3357aed55c5bb018
jaeseongDev <qkrwotjd1445@naver.com>
2021-10-04 20:31:21 +0900

    feat: CPU 부하 정도 변경, I/O 부하 코드 추가

--- src/main/java/com/example/loadtest/controller/TestController.java
+++ src/main/java/com/example/loadtest/controller/TestController.java
@@ -1,5 +1,9 @@
 package com.example.loadtest.controller;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,12 +13,35 @@ public class TestController {
     @GetMapping("/was-cpu")
     public ResponseEntity<Void> cpu() {
         String a = "a";
-        for (int i = 0; i < 100000; i++) {
+        for (int i = 0; i < 10000; i++) {
             a += i;
-            if (i % 10000 == 0) {
+            if (i % 1000 == 0) {
                 System.out.println(i);
             }
         }
+        System.out.println("완료");
+        return ResponseEntity.noContent().build();
+    }
+
+    @GetMapping("/infinite-loop")
+    public ResponseEntity<Void> infinite() {
+        boolean check = true;
+        int i = 0;
+        while(check) {
+            i += 1;
+            i = 0;
+        }
+        System.out.println(i);
+        return ResponseEntity.noContent().build();
+    }
+
+    @GetMapping("/was-io")
+    public ResponseEntity<Void> wasIo() throws IOException {
+        Path path = Paths.get("./src/main/resources/video.mp4");
+        byte[] bytes = Files.readAllBytes(path);
+
+        Path newPath = Paths.get("./src/main/resources/newVideo.mp4");
+        Files.write(newPath, bytes);
         System.out.println("완료");
         return ResponseEntity.noContent().build();
     }
