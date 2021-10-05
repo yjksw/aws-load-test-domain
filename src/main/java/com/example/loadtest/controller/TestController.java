2005fcc1fe522823536af3dc9bd9e961f25648d2
jaeseongDev <qkrwotjd1445@naver.com>
2021-10-05 10:28:53 +0900

    feat: 네트워크 부하 주는 요청 추가

--- src/main/java/com/example/loadtest/controller/TestController.java
+++ src/main/java/com/example/loadtest/controller/TestController.java
@@ -1,9 +1,11 @@
 package com.example.loadtest.controller;
 
 import java.io.IOException;
+import java.io.InputStream;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import org.apache.tomcat.util.http.fileupload.IOUtils;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -44,5 +46,13 @@ public class TestController {
         Files.write(newPath, bytes);
         System.out.println("완료");
         return ResponseEntity.noContent().build();
+    }
+
+    @GetMapping("/was-network")
+    public ResponseEntity<byte[]> wasNetwork() throws IOException {
+        Path path = Paths.get("./src/main/resources/video.mp4");
+        byte[] bytes = Files.readAllBytes(path);
+        System.out.println("완료");
+        return ResponseEntity.ok(bytes);
     }
 }
