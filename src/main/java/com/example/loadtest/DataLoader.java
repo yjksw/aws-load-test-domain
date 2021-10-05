7db056db34a57e0af022dc3ffe48f1a817c33268
jaeseongDev <qkrwotjd1445@naver.com>
2021-10-05 16:31:59 +0900

    feat: 무거운 DB 데이터 셋팅

--- src/main/java/com/example/loadtest/DataLoader.java
+++ src/main/java/com/example/loadtest/DataLoader.java
@@ -20,16 +20,21 @@ public class DataLoader implements CommandLineRunner {
 
     @Override
     public void run(String... args) {
-            logger.info("확인");
-//        if (postRepository.findAll().isEmpty()) {
-//            for (int i = 0; i < 1_000_000; i++) {
-//                Post newPost = new Post("내용내용" + i, "글쓴이" + i, "카테코리" + i);
-//                Post savedPost = postRepository.save(newPost); //flush
-//
-//                Comment comment = new Comment("댓글", "글쓴이" + i);
-//                savedPost.addComment(comment);
-//                commentRepository.save(comment);
-//            }
-//        }
+        logger.info("확인");
+        for (int i = 0; i < 1_000; i++) {
+            StringBuilder sb = new StringBuilder();
+            for (int j = 0; j < 1_000_000; j++) {
+                sb.append("내용");
+            }
+            Post newPost = new Post("내용내용" + i, "글쓴이" + i, "카테코리" + i);
+            Post savedPost = postRepository.save(newPost); //flush
+
+            Comment comment = new Comment("댓글", "글쓴이" + i);
+            savedPost.addComment(comment);
+            commentRepository.save(comment);
+            if (i % 10 == 0) {
+                System.out.println(i);
+            }
+        }
     }
 }
