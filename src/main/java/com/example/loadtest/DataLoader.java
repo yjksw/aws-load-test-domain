0daf747785c9d71b685dc78a78c3b0447b7904b3
jaeseongDev <qkrwotjd1445@naver.com>
2021-10-05 15:21:06 +0900

    refactor: DataLoader를 활용한 초기 데이터 추가

--- src/main/java/com/example/loadtest/DataLoader.java
+++ src/main/java/com/example/loadtest/DataLoader.java
@@ -1,6 +1,7 @@
 package com.example.loadtest;
 
 import com.example.loadtest.domain.Comment;
+import com.example.loadtest.domain.CommentRepository;
 import com.example.loadtest.domain.Post;
 import com.example.loadtest.domain.PostRepository;
 import lombok.RequiredArgsConstructor;
@@ -15,16 +16,18 @@ public class DataLoader implements CommandLineRunner {
 
     private final Logger logger = LoggerFactory.getLogger(DataLoader.class);
     private final PostRepository postRepository;
+    private final CommentRepository commentRepository;
 
     @Override
     public void run(String... args) {
-
-        for (int i = 0; i < 5; i++) {
+        logger.info("확인");
+        for (int i = 0; i < 1_000_000; i++) {
             Post newPost = new Post("내용내용" + i, "글쓴이" + i, "카테코리" + i);
             Post savedPost = postRepository.save(newPost); //flush
 
             Comment comment = new Comment("댓글", "글쓴이" + i);
             savedPost.addComment(comment);
+            commentRepository.save(comment);
         }
     }
 }
